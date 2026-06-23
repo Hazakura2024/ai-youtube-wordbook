4934e14b6feefa262b22aeb4fa4f678b18a28372
Hazakura2024 <ytr240.jp@gmail.com>
2026-06-23 13:02:33 +0900

    feat: 単語抽出実行部分を作成

--- src/prisma.ts
+++ src/prisma.ts
@@ -18,10 +18,13 @@ const prismaClientSingleton = () => {
   });
 };
 
+// 学習メモ: 実行環境全体で共通して使える箱の中に prismaGlobal という名前で PrismaClient インスタンスを入れておくよ」と宣言
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
+// 学習メモ: すでにグローバルに PrismaClient が存在すればそれを再利用し、なければ新規に作成する
 export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
+// 学習メモ: 開発環境のときだけ、作成した prisma インスタンスをグローバルの箱（globalThis.prismaGlobal）に避難させて再利用
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
